118d01fb77e53b546b392d2f08198fe2dc0a7690
kevin willian <kevin_willian@hotmail.com.br>
2026-03-18 17:58:44 -0300

    Create Cliente.hs

---
+++ 2026.1/linguagens de progração/testes em aula/Cliente.hs
@@ -0,0 +1,36 @@
+--aula 180326
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+module Cliente where 
+
+data Cliente = OrgGov String
+             | Empresa String Integer String String
+             | Individuo String String Bool
+             deriving Show 
+
+data Pessoa = Pessoa String String Genero 
+            deriving Show 
+
+-- (criando um 'data' "Pessoa" usando outro 'data' "Genero") ghci> Pessoa ("joao" "pedro" Masculino)
+
+data Genero = Masculino
+            | Feminino
+            | Outro 
+            deriving Show 
+
+data Expr = Btrue
+          | Bfalse
+          | And Expr Expr 
+          deriving Show 
+
+nomeCliente :: Cliente -> String
+nomeCliente c = case c of 
+                    OrgGov n -> n 
+                    Empresa n i r c -> n    --n=nome, i=id, r=responsavel, c=cargo 
+                    Individuo p ads ->      --nao sei se esta certo
+                        case p of 
+                            Pessoa n s g -> n   --n=nome, s=sobrenome, g=genero
+
+nomeCliente'' :: Cliente -> String
+nomeCliente'' (OrgGov n) = n    --n=nome
+nomeCliente'' (Empresa n _ _ _) =n 
+nomeCliente'' (Individuo (Pessoa n s _)_ _ ) = n++ " " ++s    --n=nome, s=sobrenome, _=seria o genero mas como nao é usado esta preenchendo espaco ja que nao pode estar vazio
